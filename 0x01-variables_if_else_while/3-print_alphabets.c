3e34f7ce7d1c9fbbe911d418c4b319cedcbe5f87
Sli5m <rogersbag@gmail.com>
2022-09-09 20:32:16 -0700

    .

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,10 +12,11 @@ int main(void)
 {
 	char low;
 	char upper;
+
 	for (low = 'a';  low <= 'z'; low++)
 		putchar(low);
 	for (upper = 'A'; upper <= 'Z'; upper++)
-		putchar('upper');
+		putchar(upper);
 	putchar('\n');
 
 	return (0);
