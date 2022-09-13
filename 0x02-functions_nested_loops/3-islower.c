0e90d0128c5bf6c2f50af4b5a07959ad2acb2f91
Sli5m <rogersbag@gmail.com>
2022-09-13 07:20:29 -0700

    modified is lower to describe the c parameter

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,8 +2,7 @@
 
 /**
  * _islower - checks for lowercase character
- * @c is the xter to check
- *
+ * @c: is the xter to check
  * Return: 1 if c is lowercase otherwise return 0
  */
 int _islower(int c)
